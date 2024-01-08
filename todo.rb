a0e0e4de05b8b9acf2d02b448ca3a7583b98c8f2
William <wguiraud@protonmail.com>
2024-01-08 15:15:21 -0800

    defining the data structure to represre to represent the list attribute of the session object

--- todo.rb
+++ todo.rb
@@ -3,5 +3,9 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
+  @lists = [
+    {name: "Lunch Groceries" }, 
+    {name: "Dinner Groceries" }
+  ]
   erb "You have no lists.", layout: :layout
 end

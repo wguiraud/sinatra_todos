b5e937bc6b937fe16c31e0b79ecc0ca7ba9d93de
William <wguiraud@protonmail.com>
2024-01-25 16:30:37 -0800

    implementing the delete todo route.

--- todo.rb
+++ todo.rb
@@ -118,3 +118,15 @@ post "/lists/:list_id/todos" do
 		redirect "/lists/#{@list_id}"
 	end 
 end
+
+# Delete a Todo from a list
+post "/lists/:list_id/todos/:id/delete" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:id].to_i
+
+  @list[:todos].delete_at todo_id
+  session[:success] = "The todo was successfully deleted"
+  redirect"/lists/#{@list_id}"
+end

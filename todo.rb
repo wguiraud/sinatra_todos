631975bc2af03cbe301629f898b8f4b6b1d005fa
William <wguiraud@protonmail.com>
2024-01-31 16:13:18 -0800

    implementing the check box for completed and not completed todos in the list.erb template. implmenting a new route responsible for updating the completed status of a todo item.

--- todo.rb
+++ todo.rb
@@ -130,3 +130,17 @@ post "/lists/:list_id/todos/:id/delete" do
   session[:success] = "The todo was successfully deleted"
   redirect"/lists/#{@list_id}"
 end
+
+post "/lists/:list_id/todos/:id" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:id].to_i
+
+  is_completed = params[:completed] == "true"
+  @list[:todos][todo_id][:completed] = is_completed
+
+  session[:success] = "The todo was successfully updated"
+  redirect"/lists/#{@list_id}"
+
+end
